2d80c4092d920640a75b24df6dbe79ad2e034e17
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-05-28 01:55:50 +0530

    Initial commit

---
+++ components/MainButton.ios.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+
+import Colors from "../constants/colors";
+
+const MainButton = ({ children, onPress }) => {
+  return (
+    <TouchableOpacity activeOpacity={0.6} onPress={onPress}>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 25,
+  },
+  buttonText: {
+    color: "white",
+    fontFamily: "open-sans",
+    fontSize: 18,
+  },
+});
+
+export default MainButton;
